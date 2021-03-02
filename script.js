ba423f4c6ffb984200e840a5e69ef45d59c82b76
shayshahal <shay840@gmail.com>
2021-03-02 18:20:01 +0200

    add display functionality

--- script.js
+++ script.js
@@ -24,3 +24,11 @@ function operate(x, y, op)
     }
     return decider[op](x, y);
 }
+const display = document.getElementById("display");
+function opDisplay(e)
+{
+    console.log(e.target.innerText);
+    display.innerText = display.innerText.concat(e.target.innerText);
+}
+const operators = document.querySelectorAll('button[class="op"]');
+operators.forEach(op => op.addEventListener("click", opDisplay));
