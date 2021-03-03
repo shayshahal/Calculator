80fca8919d8f0ce6f6579137e82c1f4f2f4c5164
shayshahal <shay840@gmail.com>
2021-03-03 19:52:57 +0200

    add operation display functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function add(x, y)
 {
-    return x + y;
+    return +x + +y;
 }
 function subtract(x, y)
 {
@@ -14,21 +14,67 @@ function divide(x,  y)
 {
     return x / y;
 }
-function operate(x, y, op)
+function operate(x, op, y)
 {
+    console.log(x, op, y);
     const decider = {
         '+': add, 
         '-': subtract,
-        '*': multiply,
-        '/': divide
+        '×': multiply,
+        '÷': divide
     }
     return decider[op](x, y);
 }
-const display = document.getElementById("display");
 function opDisplay(e)
 {
-    console.log(e.target.innerText);
+
     display.innerText = display.innerText.concat(e.target.innerText);
 }
+function clearDisplay()
+{
+    display.innerText =     "";
+}
+function convertMultiplyDivide(arr)
+{
+    let res = 0;
+    while(arr.length >= 3)
+    {
+        res = operate(arr.shift(), arr.shift(), arr.shift());
+        arr.unshift(res);
+    }
+    return res;
+}
+function convertToMath(str)
+{
+    let nums = str.split(/[+]|(?=[-][0-9]+)/g);
+    function mapper(x)
+    {
+        if(x.length >= 3)
+        {
+            let arr = x.split(/(?=[×÷])|(?<=[×÷])/g);
+            console.log(arr);
+            return convertMultiplyDivide(arr);
+        }
+        else
+        {
+            return x;
+        }
+    }
+    let res = nums.map(mapper);
+    return res.reduce((acc, x) => +acc + +x);
+}
+function giveResult()
+{
+    let str = display.innerText;
+    display.innerText = convertToMath(str);
+}
+const display = document.getElementById("display");
+
 const operators = document.querySelectorAll('button[class="op"]');
 operators.forEach(op => op.addEventListener("click", opDisplay));
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", clearDisplay);
+
+const equal = document.getElementById("equal");
+equal.addEventListener("click", giveResult);
